69a03630ad479114384c5907ac238ffab3247ae2
Dimasach2023 <daddymammy2023@gmail.com>
2025-12-24 15:12:41 +0300

    add

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    private int currentLevel = 0;
+    void Update()
+    {
+        if(CheckRings())
+        {
+            currentLevel++;
+            NextLevel();
+        }
+    }
+    bool CheckRings()
+    {
+        GameObject[] rings = GameObject.FindGameObjectsWithTag("Ring");
+        return rings.Length == 0;
+    }   
+    void NextLevel()
+    {
+        SceneManager.LoadScene(currentLevel);
+    }
+}
